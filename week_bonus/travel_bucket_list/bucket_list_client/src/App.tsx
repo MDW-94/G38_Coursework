f1a7b11fb564fe4471b8473c5a1747e8171ba19d
Matthew David Wasylko <matthew.wasylko@gmail.com>
2023-11-24 13:51:09 +0000

    updated testing with jest

--- week_bonus/travel_bucket_list/bucket_list_client/src/App.tsx
+++ week_bonus/travel_bucket_list/bucket_list_client/src/App.tsx
@@ -1,11 +1,33 @@
+import styled from "styled-components";
+import { useCountries } from "./hooks/useCountries";
 
-function App() {
+const LoadingStyle = styled.h2`
+justify-content: center;
+align-items: center;
+width: 20vw;
+animation: spin 2s linear infinite;
 
-  return (
-    <>
-      
-    </>
-  )
+@keyframes spin {
+  0% { transform: rotate(0deg); }
+  100% { transform: rotate(360deg); }
 }
 
-export default App
+`
+
+function App() {
+  const { isLoading, countries } = useCountries();
+
+  return isLoading ? (
+    <>
+      <h1>
+        Loading... <LoadingStyle>&#164;</LoadingStyle>
+      </h1>
+    </>
+  ) : (
+    <>
+      <pre>{JSON.stringify(countries, null, 2)}</pre>
+    </>
+  );
+}
+
+export default App;
---
+++ week_bonus/travel_bucket_list/bucket_list_client/src/components/Button/Button.test.tsx
@@ -0,0 +1,13 @@
+import { render } from "@testing-library/react";
+
+describe("Button", () => {
+  it("display a label", () => {
+    render();
+  });
+});
+
+test("maths works", () => {
+  expect(2 + 2).toEqual(4);
+});
+
+// bucket_list_client> npm test
